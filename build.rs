e433440a1d80ddae7d105819b4faa8fd4d321327
Waritnan Sookbuntherng <lion328@hotmail.co.th>
2017-11-08 13:57:42 +0700

    Fix cross platform target build (#17)

--- build.rs
+++ build.rs
@@ -1,14 +1,18 @@
 extern crate gcc;
 
+use std::env;
+
 fn main() {
-    if cfg!(target_os = "linux") {
-        gcc::compile_library("libinfo.a", &["c/linux.c"]);
-    } else if cfg!(target_os = "macos") {
-        gcc::compile_library("libinfo.a", &["c/macos.c"]);
-    } else if cfg!(target_os = "windows") {
-        gcc::compile_library("libinfo.a", &["c/windows.c"]);
-        println!("cargo:rustc-flags=-l psapi");
-    } else {
-        panic!("Unsupported system");
-    }
+    let target = env::var("TARGET").unwrap();
+    let target_os = target.split('-').nth(2).unwrap();
+
+    match target_os {
+        "linux" => gcc::compile_library("libinfo.a", &["c/linux.c"]),
+        "darwin" => gcc::compile_library("libinfo.a", &["c/macos.c"]),
+        "windows" => {
+            gcc::compile_library("libinfo.a", &["c/windows.c"]);
+            println!("cargo:rustc-flags=-l psapi");
+        },
+        _ => panic!("Unsupported system")
+    };
 }
