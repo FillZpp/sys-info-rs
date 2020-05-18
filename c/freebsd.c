6987b692263e442847287bf04cbe8485c4ef39c4
Konstantin Belousov <kib@freebsd.org>
2020-05-18 14:56:45 +0300

    Fix return type of the FreeBSD implementation of get_cpu_speed() and get_cpu_total().
    Rust code expect u64, C code returned unsigned long.  These are the same
    types only on LP64 architectures.  Use uint64_t for return type.

--- c/freebsd.c
+++ c/freebsd.c
@@ -28,7 +28,7 @@ const char *get_os_release(void) {
 	return (os_release);
 }
 
-unsigned long get_cpu_speed(void) {
+uint64_t get_cpu_speed(void) {
 	uint64_t tsc_freq;
 	size_t len;
 	int error;
@@ -40,7 +40,7 @@ unsigned long get_cpu_speed(void) {
 	return (tsc_freq / 1000 / 1000);
 }
 
-unsigned long get_proc_total(void) {
+uint64_t get_proc_total(void) {
 	struct kinfo_proc *kp, *kpp;
 	int mib[3], count, error;
 	size_t len;
